8db5309f9609e06327f8a779799552b40735481c
RolandIT <roland.szarka.it@gmail.com>
2019-12-09 19:36:21 +0100

    uloha 2 version 2

--- 10-cvicenie/uloha02/test/bom/bom.spec.0.1.3.js
+++ 10-cvicenie/uloha02/test/bom/bom.spec.0.1.3.js
@@ -22,6 +22,19 @@ describe("Remove BOM", function() {
         assert.equal(chunk.length,0);
         done();
       });
-     
-  });
+    });
+    it("RemoveBom - should not remove BOM from empty file without BOM", function(done) {
+
+        let chunks = [];
+        let file = `${__dirname}/data/with-bom-empty.txt`;
+        fs.createReadStream(file, {highWaterMark: 2})
+          .pipe(bom.remove())
+          .on("error", done)
+          .on("data", (chunk) => chunks.push(chunk))
+          .on("finish", () => {
+            let chunk = Buffer.concat(chunks);
+            assert.equal(chunk.length,0);
+            done();
+          });
+        });
 });
